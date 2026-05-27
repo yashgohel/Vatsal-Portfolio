1cd5146bf81f003e5f5557e6a720b164171e66fd
Gohel Yash <yashgohel0000@gmail.com>
2026-05-27 19:53:19 +0530

    Collect and initialize Django admin static assets and vendor ependencies

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt --break-system-packages
+python3 manage.py collectstatic --noinput
